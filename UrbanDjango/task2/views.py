b91018a4a8ed85bcbe4027cbf736391f13378dcc
53rNa <53rna76@gmail.com>
2024-12-23 15:31:55 +0300

    Исправление в классовом представлении

--- UrbanDjango/task2/views.py
+++ UrbanDjango/task2/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
-
-from django.shortcuts import render
-from django.views import View
+from django.views.generic import View, TemplateView
 
 # Классовое представление
-class ClassBasedView(View):
-    def get(self, request):
-        return render(request, 'second_task/class_view.html')
+# class ClassBasedView(View):
+#     # def get(self, request):
+#     #     return render(request, 'second_task/class_view.html')
+#     pass
+
+class ClassBasedView(TemplateView):
+    template_name = 'second_task/class_view.html'
+
 
 # Функциональное представление
 def function_based_view(request):
